fd0768023ad77331f13daefdf089a64e8e2c1b28
Aevum <jonxfrei@gmail.com>
2026-02-28 18:39:41 -0800

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,100);
+
+        int guesses = 0;
+
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            guesses++;
+
+            if (guess == magic_number)
+            {
+                Console.WriteLine("You found it!");
+
+                if (guesses == 1)
+                    Console.WriteLine($"It took you 1 guess.");
+                else
+                    Console.WriteLine($"It took you {guesses} guesses.");
+
+                Console.Write("Do you want to play again? ");
+                if (Console.ReadLine().ToLower() != "yes")
+                {
+                    break;
+                }
+
+                guesses = 0;
+                magic_number = randomGenerator.Next(1,100);
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
