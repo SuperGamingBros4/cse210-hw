9c1ae46067baf0e3a403c211e8ee756fc9cb8ef2
Aevum <jonxfrei@gmail.com>
2026-02-28 17:47:51 -0800

    Finished Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Get first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Get last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
