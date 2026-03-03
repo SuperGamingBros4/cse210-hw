264a39ad7adc00231200a60b11441964da1d7215
Aevum <jonxfrei@gmail.com>
2026-03-03 15:23:25 -0800

    Finish Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Michaelsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1995;
+        job1._endYear = 2004;
+
+        Job job2 = new Job();
+        job2._company = "Pear";
+        job2._jobTitle = "Project Manager";
+        job2._startYear = 2004;
+        job2._endYear = 2010;
+
+        Resume resume = new Resume();
+        resume._name = "David Brickham";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
