306f1764f9a692d770dd02b898f909c6742507e7
Aevum <jonxfrei@gmail.com>
2026-04-02 16:30:10 -0700

    Initial Product

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,39 @@
+public class Product
+{
+    private int _id;
+    private string _name;
+    private double _pricePerUnit;
+    private int _quantity;
+
+    public Product(int id, string name, double pricePerUnit, int quantity)
+    {
+        // Set the values of the product
+        _id = id;
+        _name = name;
+        _pricePerUnit = pricePerUnit;
+        _quantity = quantity;
+    }
+
+    public int GetID()
+    {
+        return _id;
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+    public double GetPricePerUnit()
+    {
+        return _pricePerUnit;
+    }
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        // Return the price per unit times the quantity
+        return GetPricePerUnit() * GetQuantity();
+    }
+}
