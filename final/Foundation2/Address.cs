e995b7300a25be4d03b1d55b33a68d04fc89f13b
Aevum <jonxfrei@gmail.com>
2026-04-02 17:08:40 -0700

    Initial Address

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,61 @@
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _provinceOrState; // or state
+    private string _country;
+
+    public Address(string streetAddress, string city, string provinceOrState, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _provinceOrState = provinceOrState;
+        _country = country;
+    }
+
+    public string GetStreetAddress()
+    {
+        return _streetAddress;
+    }
+    public string GetCity()
+    {
+        return _city;
+    }
+    public string GetProvince()
+    {
+        return _provinceOrState;
+    }
+    public string GetState()
+    {
+        return _provinceOrState;
+    }
+    public string GetCountry()
+    {
+        return _country;
+    }
+
+    public bool IsInUSA()
+    {
+        // Return if the country of the address is the USA
+        return GetCountry() == "USA";
+    }
+    public string GetDisplay()
+    {
+        string streetAddress = GetStreetAddress();
+        string city = GetCity();
+        if (IsInUSA()) {
+            string state = GetState();
+
+            // Return the all of the values of the address formatted
+            return $"{streetAddress}\n{city}, {state}";
+        }
+        else
+        {
+            string province = GetState();
+            string country = GetCountry();
+
+            // Return the all of the values of the address formatted
+            return $"{streetAddress}\n{city}, {province}\n{country}";
+        }
+    }
+}
