af2e55bf8952881d4d470af35d0d62730eee6151
Aevum <jonxfrei@gmail.com>
2026-04-08 03:33:13 -0700

    Initial Event & Lecture class implementation

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,37 @@
+public abstract class Event
+{
+    private string _title;
+    private string _description;
+    private string _address;
+    private string _date;
+    private string _time;
+
+    public Event(string title, string description, string address, string date, string time)
+    {
+        // Set the attributes
+        _title = title;
+        _description = description;
+        _address = address;
+        _date = date;
+        _time = time;
+    }
+
+    public string GetShortDetails()
+    {
+        // Create a short marketing description of the details based on the title, event type, and date
+        string details = $"{_title}: {GetEventType()} on {_date}";
+
+        return details;
+    }
+    public string GetDetails()
+    {
+        // Create a description of the details based on the title, address, date, time, and description of the event
+        string details = $"{_title} at {_address} on {_date} {_time}";
+        details += $"\n  {_description}";
+
+        return details;
+    }
+
+    public abstract string GetEventType();
+    public abstract string GetFullDetails();
+}
---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,26 @@
+public class Lecture : Event
+{
+    private string _speakerName;
+    private int _capacity;
+
+    public Lecture(string title, string description, string address, string date, string time, string speakerName, int capacity)
+        : base(title, description, address, date, time)
+    {
+        // Set the attributes
+        _speakerName = speakerName;
+        _capacity = capacity;
+    }
+
+    public override string GetEventType()
+    {
+        return "Lecture";
+    }
+    public override string GetFullDetails()
+    {
+        // Create a description of the details based on the standard details, the speaker, and the capacity of the event
+        string details = GetDetails();
+        details += $"\n  Speaker: {_speakerName} - Max occupancy: {_capacity}";
+
+        return details;
+    }
+}
