d12ed17fa21eea2dce588259a75bad6b9221a330
Aevum <jonxfrei@gmail.com>
2026-04-08 16:03:08 -0700

    Initial OutdoorGathering class implementation

---
+++ final/Foundation3/OutdoorGathering.cs
@@ -0,0 +1,24 @@
+public class OutdoorGathering : Event
+{
+    private string _forecast;
+
+    public OutdoorGathering(string title, string description, string address, string date, string time, string forecast)
+        : base(title, description, address, date, time)
+    {
+        // Set the attributes
+        _forecast = forecast;
+    }
+
+    public override string GetEventType()
+    {
+        return "Outdoor gathering";
+    }
+    public override string GetFullDetails()
+    {
+        // Create a description of the details based on the standard details and the forecasted weather
+        string details = GetDetails();
+        details += $"\n  Expected weather: {_forecast}";
+
+        return details;
+    }
+}
