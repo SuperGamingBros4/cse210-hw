ff1a8f5908f468be64c528176c2bf0bf10eebe40
Aevum <jonxfrei@gmail.com>
2026-04-08 21:26:41 -0700

    Foundation4 program interaction

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create a list of activities
+        List<Activity> activities = new List<Activity>();
+
+        // Create a RunningActivity
+        Activity runningActivity = new RunningActivity("24 Mar 2026", 45, 4);
+        activities.Add(runningActivity);
+        
+        // Create a CyclingActivity
+        Activity cyclingActivity = new CyclingActivity("12 Feb 2026", 30, 20);
+        activities.Add(cyclingActivity);
+        
+        // Create a SwimmingActivity
+        Activity swimmingActivity = new SwimmingActivity("16 Jun 2026", 60, 70);
+        activities.Add(swimmingActivity);
+
+        foreach (Activity activity in activities)
+        {
+            // Print a summary for each activity
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
